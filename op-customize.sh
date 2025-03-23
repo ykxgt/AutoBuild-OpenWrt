20466dc435ebe7220e0fb63c77e2e3336667a757
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-03-23 21:46:25 +0800

    Update op-customize.sh

--- op-customize.sh
+++ op-customize.sh
@@ -6,4 +6,4 @@
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # lear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/base-files/files/etc/shadow
